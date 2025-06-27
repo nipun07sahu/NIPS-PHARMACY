d2d0980668e8b4f2f8327091f0de65b676e5f776
nipun07sahu <nipunsahu44@gmail.com>
2025-06-27 11:52:29 +0530

    order.js

--- order.js
+++ order.js
@@ -1,6 +1,45 @@
+updated_order_js = """
 // Save and retrieve orders from localStorage
 let currentOrder = JSON.parse(localStorage.getItem('currentOrder')) || {};
 let orders = JSON.parse(localStorage.getItem('orders')) || [];
+
+// Product list
+const productList = [
+  "Etosaid 90mg Tab",
+  "Glucon D Plain 75gm",
+  "Cyclopam Tab",
+  "Cyclopam Susp 30ml",
+  "Clavam Drops 10ml",
+  "Paraxin 500mg Cap",
+  "Pilex Tab",
+  "Ors Pow 21.8gm",
+  "Dolo Mf Syp 120ml",
+  "Macpod 100 Oral Syp 30ml",
+  "Vertistar Md 16 Tab",
+  "Vertistar Md 8mg Tab",
+  "Cefi O 200mg Tab",
+  "Tetanus Toxoid Inj 0.5ml",
+  "Figaro Olive Oil 1 Ltr",
+  "Figava Oil 500ml",
+  "Hytone Syp",
+  "Neeri Syp 100ml",
+  "Betnovate C Ont 30gm",
+  "Dispovan 5ml",
+  "Zerodol Sp Tab",
+  "Hansaplast",
+  "Tide 10mg Tab"
+];
+
+// Populate product dropdown if on the order page
+if (document.getElementById('productSelect')) {
+  const productSelect = document.getElementById('productSelect');
+  productList.forEach(product => {
+    const option = document.createElement('option');
+    option.value = product;
+    option.textContent = product;
+    productSelect.appendChild(option);
+  });
+}
 
 // Highlight active link
 const links = document.querySelectorAll('.nav-link');
@@ -69,3 +108,11 @@ function displayOrders() {
     container.appendChild(div);
   });
 }
+"""
+
+# Save the updated order.js file
+order_js_path = '/mnt/data/order.js'
+with open(order_js_path, 'w') as file:
+    file.write(updated_order_js)
+
+order_js_path
